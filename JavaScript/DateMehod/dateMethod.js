f9593f4dcbc59977dd32a85bd0e8ce2de18bd8b3
Ajay M <ajaymari74@gmail.com>
2026-09-03 21:31:50 +0530

    fiieupdation

---
+++ JavaScript/DateMehod/dateMethod.js
@@ -0,0 +1,58 @@
+// let d = new Date()
+// // d.setFullYear(2025)
+// // console.log(d);
+// d.setMonth(11 ,23)
+// console.log(d);
+
+
+
+// let volwelCount =0;
+// for(i =0;i<=uName.length; i++){
+//     let Char = uName.charAt(i).toLowerCase();
+
+//     if(Char =='a'|| Char == 'e'|| Char == 'i'|| Char == 'o'|| Char == 'u' ){
+//         volwelCount ++;
+//         console.log(`Vowel Letter found: ${Char}. Current count: ${volwelCount}`);
+        
+//     }
+
+
+  
+        
+// }
+// console.log("Total Vowel Count is: " + volwelCount);
+// let uName = "madam"
+// let reverse =""
+
+// for(i = uName.length-1;i>=0;i--){
+//   reverse += uName.charAt(i);
+// }
+// console.log(reverse);
+// if(reverse == uName){
+//     console.log( "is Palindrome"+ " "+reverse);
+    
+// }else{
+//     console.log("is NOt Palindrome"+" "+reverse);
+// 
+// }
+
+let str ="I'm Ajay I'm From I'm Tenkasi"
+let str1 = str.trim()
+console.log(str1);
+//  let newStr = str.replaceAll(" ","_");
+//  console.log(newStr);
+ let count = str1.split("I'm")
+
+console.log( count);
+ 
+
+ 
+
+//split
+// let words = "My name is Ajay.M"
+// console.log(words.split("."));
+// console.log(words.split(" ",2));
+
+
+
+
---
+++ JavaScript/pattern.js
@@ -0,0 +1,11 @@
+for(i = 1; i< 4; i++)
+{
+    for (k =i; k<k-1;k++){
+        if(k<=i){
+            console.log("*");
+            
+        }
+        console.log("&nbsbp","&nbsp");
+        
+    }
+}
